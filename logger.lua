9a1ebf101fb0f63fbb2c783914a01dbe17e5c078
Arnie97 <arnie97@gmail.com>
2018-01-25 02:26:13 +0800

    Refactor code with assert().

--- logger.lua
+++ logger.lua
@@ -2,17 +2,12 @@ local reader = require('read14a')
 local cmds = require('commands')
 
 
-local function keyTypeID(key_type)
-    id = {A=0, B=1}
-    return id[key_type:upper()]
-end
-
-
-local function readBlock(block, key_type, key)
+local function readBlock(block, keyType, key)
+    keyTypeIDs = {A=0, B=1}
     response, err = reader.sendToDevice(Command:new{
         cmd = cmds.CMD_MIFARE_READBL,
         arg1 = block,
-        arg2 = keyTypeID(key_type),
+        arg2 = keyTypeIDs[keyType:upper()],
         arg3 = 0,
         data = key
     })
@@ -26,24 +21,22 @@ local function readBlock(block, key_type, key)
     if command.arg1 == 0 then
         return nil, 'Auth failed'
     end
-    return command.data
+    return command.data:sub(1, 16 * 2)  -- 16 bytes in one block
 end
 
 
 local function main(args)
     -- os.execute('clear')
-    info, err = reader.waitFor14443a()
-    if err then
-        print(err)
-        return
-    end
+    info = assert(reader.waitFor14443a())
+    key = 'afff416c2daf'
+
     print('Type:', info.name)
     print('UID:',  info.uid)
-    block, err = readBlock(6, 'A', 'afff416c2daf')
-    if err then
-        print(err)
-        return
-    end
+
+    block = assert(readBlock(5, 'A', key))
+    print('Card:', block:sub(14, 28))
+
+    block = assert(readBlock(6, 'A', key))
     print('Data:', block:sub(1, 16))
 end
 
