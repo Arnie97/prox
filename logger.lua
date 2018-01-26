73dd38c9a24bee12ed4c6e174f921ccb5184d3c7
Arnie97 <arnie97@gmail.com>
2018-01-27 04:34:19 +0800

    Suppress debug messages.

--- logger.lua
+++ logger.lua
@@ -50,9 +50,18 @@ local function readEmulator(block)
 end
 
 
+local function setDebugLevel(n)
+    return reader.sendToDevice(Command:new{
+        cmd = cmds.CMD_MIFARE_SET_DBGMODE,
+        arg1 = n
+    })
+end
+
+
 local function main(args)
     -- os.execute('clear')
     info = assert(reader.waitFor14443a())
+    setDebugLevel(0)
     core.console('hf mf nested o 63 A FFFFFFFFFFFF 6 A t')
 
     block = {}
@@ -61,6 +70,7 @@ local function main(args)
     block[6] = assert(readBlock(6, 'A', key))
     block[5] = assert(readBlock(5, 'A', key))
 
+    print()
     print('Type:', info.name)
     print('UID:',  info.uid)
     print('Card:', block[5](14, 28, 2))
